2c6fca67e2a38346503db7b011ba171b01d5eaef
surinder <surinderkumar.mdb@gmail.com>
2024-01-22 15:06:59 +0530

    changes for mongodb connect

--- pages/api/users/index.js
+++ pages/api/users/index.js
@@ -1,12 +1,15 @@
 import nc from "next-connect";
-const models = require('../../../lib/db/models');
-const User = models.users
+
 export const config = {
   api: {
     bodyParser: true, // Disallow body parsing, consume as stream
   }
 }
 
+const User = require('../../../lib/db/models/user.model');
+const connectDB = require("../../../lib/db/models/index");
+
+connectDB();
 
 const handler = nc({
   onError: (err, req, res, next) => {
@@ -17,22 +20,39 @@ const handler = nc({
     res.status(404).end("Page is not found");
   },
 })
-
- // ============================================================//
- // Login users
- // ============================================================//
-
+  // ============================================================//
+  // User insert record
+  // ============================================================//
   .post(async (req, res) => {
     try {
-        const newUser = await User.create( JSON.parse(req.body));
-        return res.status(200).json({
-          status: 'success',
-          message: 'done',
-          data:newUser
-        });
+      console.log(req.body, '========req.body');
+      const newUser = await User.create(JSON.parse(req.body));
+      return res.status(200).json({
+        status: 'success',
+        message: 'done',
+        data: newUser
+      });
     } catch (error) {
       console.log(error)
-       res.status(401).send({error: error})
+      res.status(401).send({ error: error })
+    }
+
+  })
+
+  // ============================================================//
+  // Get User record
+  // ============================================================//
+  .get(async (req, res) => {
+    try {
+      const newUser = await User.find();
+      return res.status(200).json({
+        status: 'success',
+        message: 'done',
+        data: newUser
+      });
+    } catch (error) {
+      console.log(error)
+      res.status(401).send({ error: error })
     }
 
   })
